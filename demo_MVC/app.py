5a7e6312bac6dc02894cb21c88280e0209b516f1
ERIK HERNANDEZ <156980862+Erik-2209@users.noreply.github.com>
2025-02-17 19:49:54 +0000

    UPDATE

--- demo_MVC/app.py
+++ demo_MVC/app.py
@@ -1,18 +1,25 @@
 import web  # Carga la librería web.py
 from controllers.index import Index
-from controllers.personas.lista_personas import ListaPersonas  # Asegúrate de que este archivo existe
+from controllers.personas.lista_personas import ListaPersonas
 from controllers.personas.insertar_personas import InsertarPersonas
 
 urls = (
     '/', 'Index',
     '/lista_personas', 'ListaPersonas',
     '/insertar_personas', 'InsertarPersonas',
+    '/favicon.ico', 'Favicon',
 )
 
 app = web.application(urls, globals())
+app = app.wsgifunc()
+
+class Favicon:
+    def GET(self):
+        web.header('Content-Type', 'image/x-icon')
+        return open('demo_MVC/favicon.ico', 'rb').read()
 
 if __name__ == "__main__":
     try:
-        app.run()
+        web.httpserver.runsimple(app, ("0.0.0.0", 8080))
     except Exception as error:
         print(f"ERROR: {str(error)}")
